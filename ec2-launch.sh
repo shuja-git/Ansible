f8679cbe600fbf9165423cb8859ac193afcc3bd4
shuja <shuja.devops@gmail.com>
2022-01-27 10:51:14 +0530

    COMPONENT

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+if [ -z "$1"  ]; then
+    echo -e "\e[1;31mInput is missing\e[0m"
+    exit 1
+fi
+COMPONENT=$1
+
+
+
 TEMP_ID="lt-06e98f843b7024a45"
 TEMP_VER=5
 
 # check if instance is already there
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance is already there"
   exit
 fi
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=front}]" | jq
+#create spot instance from launch template
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
 # update the DNS record
 
