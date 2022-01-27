33340ebca4612d1ab12d5cf39daf36d39eed869f
shuja <shuja.devops@gmail.com>
2022-01-27 10:37:18 +0530

    missed exit

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -7,6 +7,7 @@ TEMP_VER=5
 aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance is already there"
+  exit
 fi
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=front}]" | jq
 
