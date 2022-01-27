597c1862746f1a8c2057adbc76ebdf83af4baeab
shuja <shuja.devops@gmail.com>
2022-01-27 11:29:41 +0530

    update DNS record

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -10,6 +10,7 @@ COMPONENT=$1
 
 TEMP_ID="lt-06e98f843b7024a45"
 TEMP_VER=5
+ZONE_ID=Z078908237A6SF5H7OAQ8
 
 # check if instance is already there
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
@@ -20,6 +21,14 @@ fi
 #create spot instance from launch template
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
+# get private ipaddress of instance
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
+
 # update the DNS record
+sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:////tmp/record.json
+
+
 
 
